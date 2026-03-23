4419109e43f1bb90ba14e480a5a46360ed0a159a
Kacper <kacper@mac.home>
2026-03-23 22:41:01 +0100

    feat: basic navbar

--- src/layouts/chatLayout.tsx
+++ src/layouts/chatLayout.tsx
@@ -1,8 +1,10 @@
 import '../styles/chat-layout.scss';
 import ChatContainer from '../containers/chatContainer/ChatContainer';
+import Navbar from '../components/navbar/Navbar';
 const ChatLayout = () => {
   return (
     <div className='main-container'>
+      <Navbar />
       <ChatContainer />
     </div>
   )
