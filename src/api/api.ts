d82dea00040183ff92ebb89c58aad7f520d1a43c
Kacper <kacper@mac.home>
2026-03-26 10:50:49 +0100

    feat: handling api errors

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,22 +3,36 @@ export async function openApi(message: string | undefined) {
 
   const api_key = import.meta.env.VITE_OPENAI_API_KEY;
 
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${api_key}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      model: "gpt-4o-mini",
-      messages: [
-        {
-          role: "user",
-          content: message,
-        },
-      ],
-    }),
-  });
-  const data = await response.json();
-  return data.choices[0].message.content;
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${api_key}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-4o-mini",
+        messages: [
+          {
+            role: "user",
+            content: message,
+          },
+        ],
+      }),
+    });
+    
+    const data = await response.json();
+    const content = data.choices[0].message.content;
+    // handle unexpected API response (missing choices)
+    if (!content) {
+      console.error("API Error:", data);
+      return "Api error!";
+    }
+
+    return content
+    
+  } catch(error) {
+    console.error(error)
+  }
+
 }
